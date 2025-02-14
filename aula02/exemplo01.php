39435f6d3a26855f13077044bd215ed24a3ab971
evellynfurtad0 <161509297+evellynfurtad0@users.noreply.github.com>
2025-02-14 16:54:40 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - PHP</title>
+</head>
+<body>
+    <p>Oi sou a Evellyn</p>
+    <?php 
+      echo"<p>Oi sou a Evellyn</p>";
+      $nome = "Evellyn";
+      echo"<p> Oi sou a $nome </p>";
+      echo"<p> Oi sou a ".$nome."</p>"
+    ?>
+
+</body>
+</html>
